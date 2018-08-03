cc15aa2421a540ffaff5f6a293e661387cd5f780
LasingNaJedi69 <nuj101nuj@gmail.com>
2018-08-03 20:55:29 +0800

    Added materialize ui and jquery into app. Will implement angular as soon as possible.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const {app, BrowserWindow} = require('electron');
-const express = require('express');
+const components = require('./public/components.json');
+// const express = require('express');
+// const expressApp = express()
+// const path = require('path');
+
+
+// expressApp.use(express.static(path.join(__dirname, '/public')));
 
 app.on('ready', function(){
 
-    var components = require('./public/components.json');
     var win = new BrowserWindow(components);
-    win.loadFile('index.html');
-    // win.webContents.openDevTools()
+    
+    win.loadURL('file://' + __dirname + '/index.html');
+    win.webContents.openDevTools({'mode': 'detach'})
 
     win.on('closed', function(){
         win = null
     })
+    
 })
